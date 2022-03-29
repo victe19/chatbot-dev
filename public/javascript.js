5135188181f8a414577cd9f53725dd939888aa3e
victor.arauzo <victor.arauzo@rokubun.cat>
2022-03-29 23:33:27 +0200

    first commit

---
+++ public/javascript.js
@@ -0,0 +1,11 @@
+// alert('funcionando');
+
+var nav = document.getElementById("navGen");
+
+nav.addEventListener('mouseover', function(){
+    console.log("he detectado el mosueover");
+    nav.style.color = "red";
+})
+
+
+var nav = document.getElementById("navGen");
