a32f6c89d15c0fce0fadb26fdffeef3ff1c5d5dd
victe19 <victe19@gmail.com>
2022-05-11 21:55:09 +0200

    feat: [db] add firestore connection

--- db/main.py
+++ db/main.py
@@ -1,17 +1,21 @@
-from dataclasses import dataclass
-from this import d
-import pymysql
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-class DataBase:
-    def __init__(self) -> None:
-        self.connection = pymysql.connect(
-            host='localhost', #ip
-            user='root',
-            password='',
-            db='demo',
-        )
+cred = credentials.Certificate("db/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
-        self.cursor = self.connection.cursor()
-        print('Conexxió establerta')
-        
-database=DataBase()
+db = firestore.client()
+
+# Create a collection
+db.collection('people').add({'name':'Blai', 'age':42})
+
+# Add data
+data = {
+    u'name': u'Los Angeles',
+    u'state': u'CA',
+    u'country': u'USA'
+}
+
+# Add a new doc in collection 'cities' with ID 'LA'
+db.collection(u'cities').document(u'LA').set(data)
